990d9e14a31f9802834a687f57fb204674822c67
Darnycya Smith <darnycyasmith@Darnycya-Smiths-MacBook-Pro.local>
2020-07-16 16:04:04 -0400

    Alex created App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
+import Home from './screens/Home/Home'
+import User from './screens/User/User'
+// import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/user" component={User} />
+      </Switch>
     </div>
   );
 }
-
 export default App;
