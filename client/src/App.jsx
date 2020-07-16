2fe6cc0700f966757906074e7b3f569637531546
Alex Beckerman <sonificus@gmail.com>
2020-07-16 15:17:36 -0400

    basic react-app structure created in client

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import { Route, Switch } from 'react-router-dom'
+import Home from './screens/Home/Home'
+import User from './screens/User/User'
+// import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/user" component={User} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
