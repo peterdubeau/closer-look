7f99458fb05ffff9a054aa145fdc128f8cf979fe
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-16 17:34:59 -0400

    Continue refactor

--- client/src/screens/LoginSignup/LoginSignup.jsx
+++ client/src/screens/LoginSignup/LoginSignup.jsx
@@ -4,17 +4,6 @@ export default function LoginSignup() {
     return (
         <>
             
-            <div className='signup'>
-                <img src='null' />
-                <h4>CREATE AN ACCOUNT</h4>
-                <div className='full-name'>
-                    <input type='text' name='first-name' placeholder='FIRST NAME' />
-                    <input type='text' name='last-name' placeholder='LAST NAME' />
-                </div>
-                <input type='text' name='email' placeholder='EMAIL' />
-                <input type='password' name='password' placeholder='PASSWORD' />
-                <button>SIGN UP</button>
-            </div>
         </>
     )
 }
