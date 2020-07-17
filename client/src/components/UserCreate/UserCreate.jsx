5f1b1fa905ee96612b9934d79a76e61086af56ba
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-17 12:22:39 -0400

    Added className to form to preserve styling

--- client/src/components/UserCreate/UserCreate.jsx
+++ client/src/components/UserCreate/UserCreate.jsx
@@ -14,32 +14,32 @@ export default function UserCreate() {
 
   const [userState, userSetState] = useState(initialState)
 
-  const {firstName, lastName, email, password} = userState
-  
+  const { firstName, lastName, email, password } = userState
+
   function handleEventChange(e) {
-    userSetState({...userState, [e.target.name]: e.target.value})
+    userSetState({ ...userState, [e.target.name]: e.target.value })
   }
 
   let handleSubmit = async (e) => {
     e.preventDefault()
     const created = await createUser()
-    console.log("hey you clicked a button!")  
+    console.log("hey you clicked a button!")
   }
-    return (
-      <div className='signup'>
-        <form>
-            <img src='null' />
-            <h4>CREATE AN ACCOUNT</h4>
-            <div className='full-name'>
-            <input type='text' name='firstName' placeholder='FIRST NAME' value={firstName} onChange={handleEventChange}/>
-            <input type='text' name='lastName' placeholder='LAST NAME' value={lastName} onChange={handleEventChange}/>
-            </div>
-          <input type='text' name='email' placeholder='EMAIL' value={email} onChange={handleEventChange} />
-          <input type='password' name='password' placeholder='PASSWORD' value={password} onChange={handleEventChange}/>
-            <button onClick={handleSubmit}>SIGN UP</button>
-        </form>
-      </div>
-    )
+  return (
+    <div className='signup'>
+      <form className='signup'>
+        <img src='null' />
+        <h4>CREATE AN ACCOUNT</h4>
+        <div className='full-name'>
+          <input type='text' name='firstName' placeholder='FIRST NAME' value={firstName} onChange={handleEventChange} />
+          <input type='text' name='lastName' placeholder='LAST NAME' value={lastName} onChange={handleEventChange} />
+        </div>
+        <input type='text' name='email' placeholder='EMAIL' value={email} onChange={handleEventChange} />
+        <input type='password' name='password' placeholder='PASSWORD' value={password} onChange={handleEventChange} />
+        <button onClick={handleSubmit}>SIGN UP</button>
+      </form>
+    </div>
+  )
 }
 
 
