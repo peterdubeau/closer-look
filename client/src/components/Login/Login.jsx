a03b1ab2f858fd48874a114e4e6e7f16ac287b72
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-17 14:44:31 -0400

    Add styling to input and btn

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -5,10 +5,10 @@ export default function Login() {
     return (
             <div className='login'>
                 <img src='null' />
-                <h4>LOG IN TO YOUR ACCOUNT</h4>
+                <h4 className='login-header'>LOG IN TO YOUR ACCOUNT</h4>
                 <input type='text' name='email' placeholder='EMAIL' />
                 <input type='password' name='password' placeholder='PASSWORD' />
-                <button>LOG IN</button>
+                <button className='login-btn'>LOG IN</button>
             </div>
     )
 }
