326a442e2bd4c1b0118ebd0e6913b6f0171ac02e
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-17 11:39:43 -0400

    Create and style header

--- client/src/screens/LoginSignup/LoginSignup.jsx
+++ client/src/screens/LoginSignup/LoginSignup.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './LoginSignUp.css'
 import Login from '../../components/Login/Login'
 import UserCreate from '../../components/UserCreate/UserCreate'
+import Header from '../../components/shared/Header/Header'
 
 export default function LoginSignUp() {
     return (
         <>
+            <Header />
             <div className='login-signup'>
                 <Login />
                 <div className='vertical-line'></div>
