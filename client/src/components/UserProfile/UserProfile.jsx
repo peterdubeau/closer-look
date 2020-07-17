22c8c0674d0f8e235c1c01e51754a0258bbd9561
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-17 16:30:04 -0400

    Basic HTML

--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -3,8 +3,18 @@ import './UserProfile.css'
 
 export default function UserProfile() {
     return (
-        <div>
-            
+        <div className='profile'>
+            <form className='profile'>
+                <img src='null' />
+                <div className='up-full-name'>
+                    <input type='text' name='firstName' placeholder='FIRST NAME' value={firstName} onChange={handleEventChange} />
+                    <input type='text' name='lastName' placeholder='LAST NAME' value={lastName} onChange={handleEventChange} />
+                </div>
+                <input type='text' name='email' placeholder='EMAIL' value={email} onChange={handleEventChange} />
+                <input type='password' name='password' placeholder='PASSWORD' value={password} onChange={handleEventChange} />
+                <button className='update-user'>UPDATE</button>
+                <button className='delete-user'>DELETE USER</button>
+            </form>
         </div>
     )
 }
