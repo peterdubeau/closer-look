ffbc4edda2183c14ed8934902ec7f9954ddbeea7
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-20 16:55:03 -0400

    Add icons to header

--- client/src/components/shared/Header/Header.jsx
+++ client/src/components/shared/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import './Header.css'
+import Hamburger from '../../../assets/images/HamburgerMenu.png'
+import Search from '../../../assets/images/SearchIcon.png'
 
 export default class Header extends Component {
     render() {
         return (
             <div className='header'>
+                <img src={Hamburger}/>
                 <h1>CLOSER LOOK</h1>
+                <img src={Search}/>
             </div>
         )
     }
