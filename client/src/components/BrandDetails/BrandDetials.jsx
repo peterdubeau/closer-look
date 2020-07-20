daf83b2c4c6db685e6c7fdf2b4bcf7eeb3e5c0fa
Pete Du Beau <peterdubeau@yahoo.com>
2020-07-20 15:24:33 -0400

    BrandDetail Component set up

---
+++ client/src/components/BrandDetails/BrandDetials.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function BrandDetials() {
+  return (
+    <div className="cause-card">
+      <img className="people" src={} />
+      <img className="planet" src={} />
+      <img className="animals" src={} />
+      <h3>Climate</h3>
+      <p>This product contributes to climate change in the production and use of pretroleum products.</p>
+      <h3>SUSTAINABILITY</h3>
+      <p>This product uses a minor amount of non-recyclable materials.</p>
+      <h3>ANIMAL CRUELTY</h3>
+      <p></p>
+    </div>
+  )
+}
