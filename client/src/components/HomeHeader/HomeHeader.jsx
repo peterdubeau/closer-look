ea1d97a8e2e12325883c3c3042b76e880fe6c00d
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-21 15:55:40 -0400

    - Create if/else statement in Layout - Update layout

--- client/src/components/HomeHeader/HomeHeader.jsx
+++ client/src/components/HomeHeader/HomeHeader.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import './Header.css'
-import Hamburger from '../../../assets/images/HamburgerMenu.png'
-import Search from '../../../assets/images/SearchIcon.png'
+import './HomeHeader.css'
+import Hamburger from '../../assets/images/HamburgerMenu.png'
+import Search from '../../assets/images/SearchIcon.png'
+import User from '../../assets/images/UserIcon.png'
 
 export default class Header extends Component {
     render() {
@@ -9,6 +10,7 @@ export default class Header extends Component {
             <div className='header'>
                 <img className='hamburger' src={Hamburger}/>
                 <h1>CLOSER LOOK</h1>
+                <h3>find out more about the products you love.</h3>
                 <img className='search' src={Search}/>
             </div>
         )
--- client/src/components/shared/Layout/Layout.jsx
+++ client/src/components/shared/Layout/Layout.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import './Layout.css'
 import Header from '../Header/Header'
+import HomeHeader from '../../HomeHeader/HomeHeader'
 
 export default function Layout(props) {
-    return (
-        <div className='layout'>
-            <Header />
-            <div className='layout-children'>
-                {props.children}
+    let page = window.location.pathname
+
+    if ( page = '/' ) {
+        return (
+            <div className='layout'>
+                <HomeHeader />
+                <div className='layout-children'>
+                    {props.children}
+                </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className='layout'>
+                <Header />
+                <div className='layout-children'>
+                    {props.children}
+                </div>
+            </div>
+        )
+    }
 }
