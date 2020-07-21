c88766283ea42b3d9cda6c1ae8370b46fcc4aa4b
Alex Beckerman <sonificus@gmail.com>
2020-07-21 10:42:14 -0400

    updated ProductDetails screen and slight update to App.js to add route

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom'
-// import LoginSignUp from './screens/LoginSignUp/LoginSignUp'
 import UserEdit from './components/UserEdit/UserEdit'
 import User from './screens/User/User'
 import SearchResults from './screens/SearchResults/SearchResults'
+import ProductDetails from './components/ProductDetails/ProductDetails'
+// import LoginSignUp from './screens/LoginSignUp/LoginSignUp'
 // import ProductCards from './components/ProductCards/ProductCards';
 
 
 function App() {
   return (
     <div className="app">
-      <Route path='/' exact>
-        {/* <LoginSignUp /> */}
-      </Route>
+      {/* <Route path='/' exact>
+        <LoginSignUp />
+      </Route> */}
       <Route path='/api/users/:id'>
         <UserEdit component={User}/>
       </Route>
       <Route path='/api/products'>
         <SearchResults />
       </Route>
-
+      <Route path='/api/products/:id'>
+        <ProductDetails />
+      </Route>
     </div>
   );
 }
