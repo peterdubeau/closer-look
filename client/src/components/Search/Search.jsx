1764895758e98bdeed0d9f54f8fc76229c860959
Darnycya Smith <darnycyasmith@DarnycymithsMBP.lan1>
2020-07-21 16:37:44 -0400

    not done

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,36 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import './Search.css'
 import img from '../../assets/images/SearchIcon.png'
+import { getProducts } from '../../services/products'
+
 
 const Search = (props) => {
 
+  const initialState = {
+    product: [],
+    searchText: '',
+    productType: '',
+}
 
-  // handleSearch = event => {
-  //   const sort = () => this.handleSort(this.state.sortType)
-  //   const queriedProducts = this.state.allProducts.filter(product => product.name.toLowerCase().includes(event.target.value.toLowerCase()))
-  //   this.setState({ queriedProducts }, sort)
-  // }
+  const [searchState, setSearchState] = useState(initialState)
 
-  // handleSubmit = event => event.preventDefault()
+  const { type } = searchState
 
+  const handleChange = (e) => {
+    setSearchState({
+      searchText: e.target.value
+    })
+  }
 
+  const handleSearch = async (e) => {
+    const get = await getProducts()
+    setSearchState({
+      ...searchState,
+      productType: get.type
+    })
+  }
+  useEffect(() => {
+    handleSearch()
+    console.log(searchState)
+  }, [])
 
 
 
   return (
     <div className='search-div'>
-      <img className='search-image' src={img}/>
-      <form className="search-form">
-        <input
+     <img onClick={handleSearch} className='search-image' src={img}/>
+     <form className="search-form">
+     <Link to={`/products/${searchState.searchText}`}><input
           className="search-input"
           value={props.value}
+          onChange={handleSearch}
           name="Search"
           placeholder='SEARCH'
           type="text"
           autoFocus
-        />
+        /></Link>
       </form>
     </div>
+       
   )
 }
 
