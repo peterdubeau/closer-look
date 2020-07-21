59c06913311f1bd7e2435786c078c3f27596610c
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-21 15:57:36 -0400

    Refactor code

--- client/src/components/HomeHeader/HomeHeader.jsx
+++ client/src/components/HomeHeader/HomeHeader.jsx
@@ -7,11 +7,13 @@ import User from '../../assets/images/UserIcon.png'
 export default class Header extends Component {
     render() {
         return (
-            <div className='header'>
-                <img className='hamburger' src={Hamburger}/>
-                <h1>CLOSER LOOK</h1>
-                <h3>find out more about the products you love.</h3>
-                <img className='search' src={Search}/>
+            <div className='home-header'>
+                <img className='hamburger' src={Hamburger} />
+                <div>
+                    <h1>CLOSER LOOK</h1>
+                    <h3>find out more about the products you love.</h3>
+                </div>
+                <img className='search' src={Search} />
             </div>
         )
     }
