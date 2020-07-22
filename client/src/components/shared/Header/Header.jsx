c47f7d68296500ad98ec598e587001ceb3310341
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-22 11:05:50 -0400

    Frick

--- client/src/components/shared/Header/Header.jsx
+++ client/src/components/shared/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Header.css'
 import Hamburger from '../../../assets/images/HamburgerMenu.png'
 import Search from '../../../assets/images/SearchIcon.png'
@@ -6,8 +6,11 @@ import User from '../../../assets/images/UserIcon.png'
 import { Link } from 'react-router-dom'
 
 
-export default class Header extends Component {
-    render() {
+export default function Header() {
+
+    let page = window.location.pathname
+
+    if (page === '/api/users/') {
         return (
             <div className='header'>
                 <img className='hamburger' src={Hamburger} />
@@ -15,10 +18,21 @@ export default class Header extends Component {
                     <h1>CLOSER LOOK</h1>
                 </Link>
                 <img className='search' src={Search} />
-                <Link to= {`api/user/`}>
+            </div>
+        )
+    } else {
+        return (
+            <div className='header'>
+                <img className='hamburger' src={Hamburger} />
+                <Link className='home-link' to='/'>
+                    <h1>CLOSER LOOK</h1>
+                </Link>
+                <img className='search' src={Search} />
+                <Link to={`api/user/`}>
                     <img className='user' src={User} />
                 </Link>
             </div>
         )
     }
+
 }
