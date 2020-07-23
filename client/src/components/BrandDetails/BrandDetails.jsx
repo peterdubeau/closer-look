1e9afe63ad2ebf6f75e39094e1914f5824827d6b
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-23 12:59:03 -0400

    BrandDetails media queries

--- client/src/components/BrandDetails/BrandDetails.jsx
+++ client/src/components/BrandDetails/BrandDetails.jsx
@@ -13,30 +13,30 @@ export default function BrandDetails() {
         <img className="cause-icon" id='planet' src={Globe} />
         <img className="cause-icon" it='animal' src={Animal} />
       </div>
-    <div className ="cause-details">
-      <h3>CLIMATE</h3>
-      <p>This product contributes to climate change in the production and use of pretroleum products.</p>
-    </div> 
-    <div className ="cause-details">
-      <h3>SUSTAINABILITY</h3>
-      <p>This product uses a minor amount of non-recyclable materials.</p>
+      <div className="cause-details">
+        <h3>CLIMATE</h3>
+        <p>This product contributes to climate change in the production and use of pretroleum products.</p>
+      </div>
+      <div className="cause-details">
+        <h3>SUSTAINABILITY</h3>
+        <p>This product uses a minor amount of non-recyclable materials.</p>
+      </div>
+      <div className="cause-details">
+        <h3>ANIMAL CRUELTY</h3>
+        <p>This product is animal cruelty free</p>
+      </div>
+      <div className="cause-details">
+        <h3>HABITAT DESTRUCTION</h3>
+        <p>This product does not directly contribute to poor land use practices.</p>
+      </div>
+      <div className="cause-details">
+        <h3>LABOR LAWS</h3>
+        <p>Because of multiple parts origin we cannot say with certainty that this product is labor friendly.</p>
+      </div>
+      <div className="cause-details">
+        <h3>POLITICAL</h3>
+        <p>This product does not contribute to geopolitical stratification of resources.</p>
+      </div>
     </div>
-    <div className ="cause-details">
-      <h3>ANIMAL CRUELTY</h3>
-      <p>This product is animal cruelty free</p>
-    </div>
-    <div className ="cause-details">
-      <h3>HABITAT DESTRUCTION</h3>
-      <p>This product does not directly contribute to poor land use practices.</p>
-    </div>
-    <div className ="cause-details">
-      <h3>LABOR LAWS</h3>
-      <p>Because of multiple parts origin we cannot say with certainty that this product is labor friendly.</p>
-    </div>
-    <div className="cause-details">
-      <h3>POLITICAL</h3>
-      <p>This product does not contribute to geopolitical stratification of resources.</p>
-    </div>
- </div> 
- )
+  )
 }
